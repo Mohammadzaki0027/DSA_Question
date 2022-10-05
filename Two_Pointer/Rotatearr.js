125aee8be6e32a7a0ac1c744d923039c3fb47a73
Mohammadzaki0027 <zaki7875@gmail.com>
2022-10-05 12:16:36 +0530

    rotatearray

--- Two_Pointer/Rotatearr.js
+++ Two_Pointer/Rotatearr.js
@@ -1,47 +1,60 @@
-function pair(tc,N,K,arr)
-{
-var len=arr.length;
-K=K%len;
-reverse(0,len-K-1,arr);
-reverse(len-K,len-1,arr);
-reverse(0,len-1,arr);
-return arr
+// let arr1 = [1, 2, 3, 4, 5];
+// let R = 2;
+// let output = [4, 5, 1, 2, 3];
+
+// function rot(arr, R) {
+//   R = R % arr.length;
+
+//   let c = arr.length - R;
+
+//   for (let i = arr.length - 1; i >= c; i--) {
+//     let y = arr.pop();
+//     arr.unshift(y);
+//   }
+//   console.log(arr);
+// }
+// rot(arr1, 2);
+
+function pair(tc, N, K, arr) {
+  var len = arr.length;
+  K = K % len;
+  reverse(0, len - K - 1, arr);
+  reverse(len - K, len - 1, arr);
+  reverse(0, len - 1, arr);
+  return arr;
 }
-function reverse(start,end,arr)
-{
-    while(start<end)
-    {
-        temp=arr[start]
-        arr[start]=arr[end]
-        arr[end]=temp;
-        start++;
-        end--
-    }
-}
-
-
-
-
-
-function runProgram(input){
-    input=input.trim().split("\n")
-    var tc=Number(input[0])
-    var line=1;
-    for(var i=0;i<tc;i++)
-    {
-        var [N,K]=input[line].trim().split(" ").map(Number)
-        line++;
-var arr=input[line].trim().split(" ").map(Number)
-line++;
-
-var x=(pair(tc,N,K,arr))
-console.log(x.join(" "))
-
+function reverse(start, end, arr) {
+  while (start < end) {
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
 }
 
+function runProgram(input) {
+  input = input.trim().split("\n");
+  var tc = Number(input[0]);
+  var line = 1;
+  for (var i = 0; i < tc; i++) {
+    var [N, K] = input[line].trim().split(" ").map(Number);
+    line++;
+    var arr = input[line].trim().split(" ").map(Number);
+    line++;
+
+    var x = pair(tc, N, K, arr);
+    // console.log(x.join(" "));
+  }
 }
-if (process.env.USER === "") {
-  runProgram(``);
+if (process.env.USERNAME === "sw") {
+  runProgram(`3
+  3 2
+1 2 3 4 5
+  2 2
+  1 2
+  2 3
+  1 2`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
