9449636801c150a761b80c88e0e8a20bf0930a9c
Mohammadzaki0027 <zaki7875@gmail.com>
2022-10-14 19:36:43 +0530

    upperbond

--- Binary search/nooftimes.js
+++ Binary search/nooftimes.js
@@ -1,37 +1,42 @@
 function frequency(N, K, arr) {
-  let l = 0;
-  let r = arr.length - 1;
-  let count = 0;
-  let ans=0
-  while (l <= r) {
-    mid = l + Math.floor((r - 1) / 2);
-    if (arr[mid] === K) {
-      ans=mid
-      
-      r = mid - 1;
-    } else if (arr[mid] > K) {
-      r = mid - 1;
-    } else {
-      l = mid + 1;
+  function left(N, K, arr) {
+    let l = 0;
+    let r = arr.length - 1;
+    let count = 0;
+    let ans = 0;
+    while (l <= r) {
+      mid = l + Math.floor((r - 1) / 2);
+      if (arr[mid] === K) {
+        ans = mid;
+
+        r = mid - 1;
+      } else if (arr[mid] > K) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
     }
   }
 
-  let ul = 0;
-  let ur = arr.length - 1;
-  let uans=0
-  while (ul <= ur) {
-    mid = ul + Math.floor((ur - ul) / 2);
-    if (arr[mid] === K) {
-      ul = mid + 1;
-    uans=mid
-    } else if (arr[mid] > K) {
-      ur = mid - 1;
-    } else {
-      ul = mid + 1;
+  function rigthside(N, K, arr) {
+    let ul = 0;
+    let ur = arr.length - 1;
+    let uans = 0;
+    while (ul <= ur) {
+      mid = ul + Math.floor((ur - ul) / 2);
+      if (arr[mid] === K) {
+        ul = mid + 1;
+        uans = mid;
+      } else if (arr[mid] > K) {
+        ur = mid - 1;
+      } else {
+        ul = mid + 1;
+      }
     }
+    return uans - ans + 1;
   }
-  return (uans-ans+1);
 }
+
 function runProgram(input) {
   input = input.trim().split("\n");
   let [N, K] = input[0].trim().split(" ").map(Number);
