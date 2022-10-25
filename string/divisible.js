91d7763d09a49f6a35884ec6eaf11272fec09960
Mohammadzaki0027 <zaki7875@gmail.com>
2022-10-25 16:24:15 +0530

    rank

--- string/divisible.js
+++ string/divisible.js
@@ -1,14 +1,14 @@
-let n=2
-let num=12
-// let input=1012
-// while(num!=0)
-// {
-
-
-
-//     let nums=1012%10
-// num=
-// console.log(nums);
-// }
-
-rem=Math.floor(1012/10)
+let n = 12;
+let count = 0;
+let rem = n;
+while (rem > 0) {
+  let modvalue = rem % 10;
+  rem = Math.floor(rem / 10);
+  checkfunction(n, modvalue);
+}
+function checkfunction(n, modvalue) {
+  if (n % modvalue === 0) {
+    count++;
+  }
+}
+console.log(count);
